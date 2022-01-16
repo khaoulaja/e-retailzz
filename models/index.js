dad3efae9d5cd06252a193e98ca892d6d9ea0d0a
khaoula <khaoulajabour4@gmail.com>
2022-01-16 03:59:24 -0500

    add product/tag associations

--- models/index.js
+++ models/index.js
@@ -13,8 +13,16 @@ Category.hasMany(Product,{
   foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: 'product_tag',
+  foreignKey: 'tag_id'
+});
+
 
 module.exports = {
   Product,
